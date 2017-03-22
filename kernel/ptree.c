f4e52a79019aa724b146389255752cf8fc9b114c
norang <noranglemon@gmail.com>
2017-03-23 00:43:42 +0900

    fix dfs bug:bug:

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -18,11 +18,14 @@
 
 struct task_struct* get_first_child(struct task_struct *task)
 {
-	return list_first_entry_or_null(&task->children, struct task_struct, children);
+	return list_first_entry_or_null(&task->children, struct task_struct, sibling);
 }
 
 struct task_struct* get_next_sibling(struct task_struct *task)
 {
+	struct task_struct *first_sibling = list_entry(&task->parent->children, struct task_struct, sibling);
+	if (first_sibling == list_entry(task->sibling.next, struct task_struct, sibling))
+		return NULL;
 	return list_first_entry_or_null(&task->sibling, struct task_struct, sibling);
 }
 
@@ -37,12 +40,14 @@ struct task_struct* get_next_struct(struct task_struct *task)
 	struct task_struct *next = get_first_child(task);
 	if (next)
 		return next;
-	while (task != &init_task)
+	while (task->pid)
 	{
 		next = get_next_sibling(task);
-		if (next)
+		if (next && next->pid)
 			return next;
-		task = task->real_parent;
+		if (next && !next->pid)
+			return NULL;
+		task = task->parent;
 	}
 	return NULL;
 }
@@ -66,25 +71,31 @@ long do_ptree(struct prinfo *kbuf, int *nr_value)
 	 */
 	long entry_num = 0;
 	struct task_struct *curr_task;
+	printk(KERN_ALERT"**** do_ptree called\n");
 	read_lock(&tasklist_lock);
+	printk(KERN_ALERT"**** read locked\n");
 	curr_task = &init_task;
 	while (curr_task)
 	{
+		printk(KERN_ALERT"**** [%ld] %s: %d\n", entry_num, curr_task->comm, curr_task->pid);
 		if (entry_num < *nr_value)
 		{
 			kbuf[entry_num].state = curr_task->state;
 			kbuf[entry_num].pid = curr_task->pid;
 			kbuf[entry_num].parent_pid = curr_task->real_parent->pid;
 			kbuf[entry_num].first_child_pid = get_pid_from_task(get_first_child(curr_task));
+			printk(KERN_ALERT"**** child's pid is [%d]\n", kbuf[entry_num].first_child_pid);
+			kbuf[entry_num].state = curr_task->state;
 			kbuf[entry_num].next_sibling_pid = get_pid_from_task(get_next_sibling(curr_task));
+			printk(KERN_ALERT"**** sibling's pid is [%d]\n", kbuf[entry_num].next_sibling_pid);
 			kbuf[entry_num].uid = task_uid(curr_task);
 			strncpy(kbuf[entry_num].comm, curr_task->comm, 64);
-
-			curr_task = get_next_struct(curr_task);
 		}
+		curr_task = get_next_struct(curr_task);
 		entry_num++;
 	}
 	read_unlock(&tasklist_lock);
+	printk(KERN_ALERT"**** read unlocked\n");
 	
 
 	if (*nr_value > entry_num) *nr_value = entry_num;
@@ -109,8 +120,8 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
 	 */
 	int nr_value;
 	struct prinfo *kbuf;
-	long result;
-
+	long result = 0;
+	printk(KERN_ALERT"**** ptree called\n");
 	if (buf == NULL || nr == NULL)
 		return -EINVAL;
 	if (get_user(nr_value, nr))
@@ -119,9 +130,11 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
 		return -EINVAL;
 	
 	kbuf = (struct prinfo*)kmalloc(nr_value * sizeof(struct prinfo), GFP_KERNEL);
+	printk(KERN_ALERT"**** kbuf allocated\n");
 	if (kbuf == NULL)
 		return -ENOMEM;
 	result = do_ptree(kbuf, &nr_value);
+	printk(KERN_ALERT"**** do_ptree end\n");
 
 	if (copy_to_user(buf, kbuf, nr_value*sizeof(struct prinfo)))
 		return -EFAULT;
