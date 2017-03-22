37a6b43934cc6ddcd36e0b52b9fecb01ec452a0f
norang <noranglemon@gmail.com>
2017-03-22 22:13:04 +0900

    fill the buffer with prinfo struct in DFS preorder

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -8,12 +8,54 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/cred.h>
+#include <linux/string.h>
 
 #include <linux/prinfo.h>
 
 #include <uapi/asm-generic/errno-base.h>
 
-long do_ptree(struct prinfo *kbuf, int *nr_value){
+struct task_struct* get_first_child(struct task_struct *task)
+{
+	return list_first_entry_or_null(&task->children, struct task_struct, children);
+}
+
+struct task_struct* get_next_sibling(struct task_struct *task)
+{
+	return list_first_entry_or_null(&task->sibling, struct task_struct, sibling);
+}
+
+struct task_struct* get_next_struct(struct task_struct *task)
+{
+	/*
+	 * return value:
+	 *   (if has) child -> sibling -> parent's sibling -> ...
+	 *   (if not) null
+	 */
+	
+	struct task_struct *next = get_first_child(task);
+	if (next)
+		return next;
+	while (task != &init_task)
+	{
+		next = get_next_sibling(task);
+		if (next)
+			return next;
+		task = task->real_parent;
+	}
+	return NULL;
+}
+
+pid_t get_pid_from_task(struct task_struct *task)
+{
+	if (task == NULL)
+		return 0;
+	return task->pid;
+}
+
+long do_ptree(struct prinfo *kbuf, int *nr_value)
+{
 	/*
 	 * push prinfo struct to kbuf in Depth First Search order.
 	 * modify nr_value, 
@@ -23,8 +65,25 @@ long do_ptree(struct prinfo *kbuf, int *nr_value){
 	 * 	-EFAULT: if buf are outside the accessible address space
 	 */
 	long entry_num = 0;
+	struct task_struct *curr_task;
 	read_lock(&tasklist_lock);
-		
+	curr_task = &init_task;
+	while (curr_task)
+	{
+		if (entry_num < *nr_value)
+		{
+			kbuf[entry_num].state = curr_task->state;
+			kbuf[entry_num].pid = curr_task->pid;
+			kbuf[entry_num].parent_pid = curr_task->real_parent->pid;
+			kbuf[entry_num].first_child_pid = get_pid_from_task(get_first_child(curr_task));
+			kbuf[entry_num].next_sibling_pid = get_pid_from_task(get_next_sibling(curr_task));
+			kbuf[entry_num].uid = task_uid(curr_task);
+			strncpy(kbuf[entry_num].comm, curr_task->comm, 64);
+
+			curr_task = get_next_struct(curr_task);
+		}
+		entry_num++;
+	}
 	read_unlock(&tasklist_lock);
 	
 
