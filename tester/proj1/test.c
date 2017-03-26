6b270f2e28732ac0decc576e7561bd57496b06c4
ntopia <ntopia.p@gmail.com>
2017-03-26 14:29:24 +0900

    add logic to input maximum of number of processes

--- tester/proj1/test.c
+++ tester/proj1/test.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/unistd.h>
 #include "test.h"
 
-void print_prinfo(struct prinfo *pr, int indent) {
-	for (int i = 0; i < indent; ++i) {
+void print_prinfo(struct prinfo *pr, int indent)
+{
+	for (int i = 0; i < indent; ++i)
 		printf("  ");
-	}
+
 	printf("%s,%d,%ld,%d,%d,%d,%ld\n",
 		pr->comm, pr->pid, pr->state,
 		pr->parent_pid, pr->first_child_pid, pr->next_sibling_pid, pr->uid);
 }
 
-int main() {
-	int nr = 3;
+int main(int argc, char **argv)
+{
+	int nr = 80;
+	if (argc > 1)
+		nr = atoi(argv[1]);
+
 	struct prinfo *prs = calloc(nr, sizeof(struct prinfo));
 
-	int err = syscall(__NR_ptree, prs, &nr);
-	if (err < 0) {
+	int ret = syscall(__NR_ptree, prs, &nr);
+	if (ret < 0) {
 		free(prs);
-		printf("return with error value : %d\n", err);
+		printf("return with error value : %d, %s\n", errno, strerror(errno));
 		return 1;
 	}
+	printf("number of processes : %d\n", ret);
 
 	pid_t *pid_stack = calloc(nr, sizeof(pid_t));
 	int top = 0;
