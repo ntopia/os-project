fd8dc8dcd9d1b6964233f254aab0e4e357574887
kazel1990 <kazel1990@gmail.com>
2017-04-07 14:12:16 +0900

    add global variable that stores current rotation
    TODO: it is not thread safe

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -3,13 +3,22 @@
 
 #include <uapi/asm-generic/errno-base.h>
 
+int cur_rotation; /* current rotation of device */
+
 /*
  * set rotation function
  */
 SYSCALL_DEFINE1(set_rotation, int, degree)
 {
+	if(degree < 0 || degree >= 360)
+		return -EINVAL;
+	/*
+	 * need a lock here
+	 */
+	cur_rotation = degree;
+	printk("set rotation to %d\n",cur_rotation);
 	/*
 	 * TODO: do sth here
 	 */
-	 return 0;
+	return 0;
 }
