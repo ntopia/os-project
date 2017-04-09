832b42d3037eb55808e202ef52ec38069d700dac
norang <noranglemon@gmail.com>
2017-04-09 19:35:33 +0900

    add test files

---
+++ tester/test.h
@@ -0,0 +1,9 @@
+#ifndef _PROJ2_TEST_H
+#define _PROJ2_TEST_H
+
+#define __NR_rotlock_read	381
+#define __NR_rotlock_write	382
+#define __NR_rotunlock_read	383
+#define __NR_rotunlock_write	385
+
+#endif
