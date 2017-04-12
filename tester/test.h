512ff5fd46b839e26f2e92bca5f7ab3472baf93b
norang <noranglemon@gmail.com>
2017-04-12 23:39:25 +0900

    add set_rotation to test

--- tester/test.h
+++ tester/test.h
@@ -1,9 +1,10 @@
 #ifndef _PROJ2_TEST_H
 #define _PROJ2_TEST_H
 
-#define __NR_rotlock_read	381
-#define __NR_rotlock_write	382
-#define __NR_rotunlock_read	383
-#define __NR_rotunlock_write	385
+#define __NR_set_rotation       380
+#define __NR_rotlock_read       381
+#define __NR_rotlock_write      382
+#define __NR_rotunlock_read     383
+#define __NR_rotunlock_write    385
 
 #endif
