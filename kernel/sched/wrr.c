b5fe3342a7aad739031ba5d8ec60614158ef2226
norang <noranglemon@gmail.com>
2017-05-06 04:17:27 +0900

    make skeletons

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -6,3 +6,29 @@ void init_wrr_rq(struct wrr_rq *wrr_rq, struct rq *rq)
 {
 	raw_spin_lock_init(&wrr_rq->wrr_lock);
 }
+
+/*
+ * Set the SCHED_WRR weight of process, as identified by 'pid'.
+ * If 'pid' is 0, set the weight for the calling process.
+ * System call number 380.
+ */
+SYSCALL_DEFINE2(sched_setweight, pid_t, pid, int, weight)
+{
+	/* check task's policy */
+	/* check authority */
+	/* update the weight */
+	return 0;
+}
+
+/*
+ * Obtain the SCHED_WRR weight of a process as identified by 'pid'.
+ * If 'pid' is 0, return the weight of the calling process.
+ * System call number 381.
+ */
+SYSCALL_DEFINE1(sched_getweight, pid_t, pid)
+{
+	/* check task's policy */
+	/* return the value */
+	return 0;
+}
+
