4212137764215e5e68c1bb05ca39724fa23d64cc
ntopia <ntopia.p@gmail.com>
2017-05-07 21:22:02 +0900

    add wrr_sched_class & make their connections (rt -> wrr -> fair)

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -32,6 +32,40 @@ void init_wrr_rq(struct wrr_rq *wrr_rq, struct rq *rq)
 	raw_spin_lock_init(&wrr_rq->wrr_lock);
 }
 
+
+const struct sched_class wrr_sched_class = {
+	.next			= &fair_sched_class,
+//	.enqueue_task		= enqueue_task_wrr,
+//	.dequeue_task		= dequeue_task_wrr,
+//	.yield_task		= yield_task_wrr,
+
+//	.check_preempt_curr	= check_preempt_curr_wrr,
+
+//	.pick_next_task		= pick_next_task_wrr,
+//	.put_prev_task		= put_prev_task_wrr,
+
+#ifdef CONFIG_SMP
+//	.select_task_rq		= select_task_rq_wrr,
+
+//	.set_cpus_allowed	= set_cpus_allowed_wrr,
+//	.rq_online		= rq_online_wrr,
+//	.rq_offline		= rq_offline_wrr,
+//	.pre_schedule		= pre_schedule_wrr,
+//	.post_schedule		= post_schedule_wrr,
+//	.task_woken		= task_woken_wrr,
+//	.switched_from		= switched_from_wrr,
+#endif
+
+//	.set_curr_task		= set_curr_task_wrr,
+//	.task_tick		= task_tick_wrr,
+
+//	.get_rr_interval	= get_rr_interval_wrr,
+
+//	.prio_changed		= prio_changed_wrr,
+//	.switched_to		= switched_to_wrr,
+};
+
+
 /*
  * Set the SCHED_WRR weight of process, as identified by 'pid'.
  * If 'pid' is 0, set the weight for the calling process.
