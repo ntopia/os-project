2b0a868b50cf5c918caba6f19643e19448e4be4b
ntopia <ntopia.p@gmail.com>
2017-05-11 05:34:03 +0900

    return 0 for WRR in sched_get_priority_max/min
    We don't use 'priority' in WRR.
    Therefore, we just return 0 like SCHED_NORMAL.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4596,6 +4596,7 @@ SYSCALL_DEFINE1(sched_get_priority_max, int, policy)
 	case SCHED_NORMAL:
 	case SCHED_BATCH:
 	case SCHED_IDLE:
+	case SCHED_WRR:
 		ret = 0;
 		break;
 	}
@@ -4621,6 +4622,7 @@ SYSCALL_DEFINE1(sched_get_priority_min, int, policy)
 	case SCHED_NORMAL:
 	case SCHED_BATCH:
 	case SCHED_IDLE:
+	case SCHED_WRR:
 		ret = 0;
 	}
 	return ret;
