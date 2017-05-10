2dfb0f3f62e56b6070b9266ab1a62dedf5c8a77f
ntopia <ntopia.p@gmail.com>
2017-05-11 05:40:47 +0900

    add empty sched functions because of null pointer exception

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -200,6 +200,54 @@ static void check_preempt_curr_wrr(struct rq *rq, struct task_struct *p, int fla
 }
 
 
+#ifdef CONFIG_SMP
+
+static int select_task_rq_wrr(struct task_struct *p, int sd_flag, int flags)
+{
+	return task_cpu(p);
+}
+
+static void rq_online_wrr(struct rq *rq)
+{
+}
+
+static void rq_offline_wrr(struct rq *rq)
+{
+}
+
+static void pre_schedule_wrr(struct rq *rq, struct task_struct *prev)
+{
+}
+
+static void post_schedule_wrr(struct rq *rq)
+{
+}
+
+static void task_woken_wrr(struct rq *rq, struct task_struct *p)
+{
+}
+
+static void switched_from_wrr(struct rq *rq, struct task_struct *p)
+{
+}
+
+#endif /* CONFIG_SMP */
+
+
+static unsigned int get_rr_interval_wrr(struct rq *rq, struct task_struct *task)
+{
+	return calc_wrr_time_slice(task->wrr.weight);
+}
+
+static void prio_changed_wrr(struct rq *rq, struct task_struct *p, int oldprio)
+{
+}
+
+static void switched_to_wrr(struct rq *rq, struct task_struct *p)
+{
+}
+
+
 const struct sched_class wrr_sched_class = {
 	.next			= &fair_sched_class,
 	.enqueue_task		= enqueue_task_wrr,
@@ -212,24 +260,23 @@ const struct sched_class wrr_sched_class = {
 	.put_prev_task		= put_prev_task_wrr,
 
 #ifdef CONFIG_SMP
-//	.select_task_rq		= select_task_rq_wrr,
+	.select_task_rq		= select_task_rq_wrr,
 
-//	.set_cpus_allowed	= set_cpus_allowed_wrr,
-//	.rq_online		= rq_online_wrr,
-//	.rq_offline		= rq_offline_wrr,
-//	.pre_schedule		= pre_schedule_wrr,
-//	.post_schedule		= post_schedule_wrr,
-//	.task_woken		= task_woken_wrr,
-//	.switched_from		= switched_from_wrr,
+	.rq_online		= rq_online_wrr,
+	.rq_offline		= rq_offline_wrr,
+	.pre_schedule		= pre_schedule_wrr,
+	.post_schedule		= post_schedule_wrr,
+	.task_woken		= task_woken_wrr,
+	.switched_from		= switched_from_wrr,
 #endif
 
 	.set_curr_task		= set_curr_task_wrr,
 	.task_tick		= task_tick_wrr,
 
-//	.get_rr_interval	= get_rr_interval_wrr,
+	.get_rr_interval	= get_rr_interval_wrr,
 
-//	.prio_changed		= prio_changed_wrr,
-//	.switched_to		= switched_to_wrr,
+	.prio_changed		= prio_changed_wrr,
+	.switched_to		= switched_to_wrr,
 };
 
 
