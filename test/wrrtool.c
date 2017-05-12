aa33bc35398afcd72f07d286fad5ea7b6d19fdaa
norang <noranglemon@gmail.com>
2017-05-13 00:13:13 +0900

    make wrrtool

---
+++ test/wrrtool.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/unistd.h>
+#include <linux/sched.h>
+#include <sched.h>
+#include "test.h"
+
+#define SCHED_WRR	6
+
+int i, j;
+long ret;
+struct sched_param *param;
+char cmd[100];
+const char *helps = "\t\t=== commands ===\n"
+"  SPW [pid]\t\t: Set [pid] Policy Wrr\n"
+"  SW [pid] [new_weight]\t: Set [pid] Weight as [new_weight]\n"
+"  GW [pid]\t\t: Get Weight of [pid]\n"
+"  H\t\t\t: Help\n"
+"  Q (or E)\t\t: Quit\n";
+
+int main()
+{
+	while (1) {
+		printf("\t> input\t: ");
+		scanf("%s", cmd);
+
+		for (int i = 0; cmd[i] != '\0'; i++) {
+			if (cmd[i] >= 'a') cmd[i] -= 32;
+		}
+
+		if (cmd[0] == 'H') {
+			printf("%s", helps);
+			continue;
+		} else if (cmd[0] == 'Q' || cmd[0] == 'E') {
+			printf("\t> exit\n");
+			break;
+		} else {
+			ret = 0;
+			if (strncmp(cmd, "SPW", 3) == 0) {
+				param->sched_priority = 0;
+				scanf("%d", &i);
+				printf("  > Set Policy Wrr - pid [%d]\n", i);
+				ret = sched_setscheduler(i, SCHED_WRR, param);
+			} else if (strncmp(cmd, "SW", 2) == 0 ) {
+				scanf("%d %d", &i, &j);
+				printf("  > Set Weight - pid [%d] weight [%d]\n", i, j);
+				ret = syscall(__NR_sched_setweight, i);
+			} else if (strncmp(cmd, "GW", 2) == 0) {
+				scanf("%d", &i);
+				printf("  > Get Weight - weight [%d]\n", i);
+				ret = syscall(__NR_sched_getweight, i);
+			} else {
+				printf("  > invalid command: [%s]\n", cmd);
+				continue;
+			}
+			printf("  > return value: %ld\n", ret);
+		}
+	}
+
+	return 0;
+}
